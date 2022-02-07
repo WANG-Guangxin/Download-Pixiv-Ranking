52e60e7b3d643e817cc1a94a8a48fe0a9a273d91
Guangxin Wang (汪广鑫) <71872849+WANG-Guangxin@users.noreply.github.com>
2022-02-07 18:53:41 +0800

    Update gen_readme.py

--- gen_readme.py
+++ gen_readme.py
@@ -3,7 +3,9 @@ import os
 readme = \
 '''
 <p align = "center">
-<h2 align="center">✨ Pixiv 日榜 ✨</h2><br>
+<h2 align="center">✨ Pixiv 日榜 ✨</h2>
+</p>
+<p align = "center">
 '''
 
 list_dir = os.listdir('./Pixiv日榜')
@@ -11,7 +13,7 @@ file_list = os.listdir('./Pixiv日榜/'+list_dir[0]+'/')
 
 count = 0
 for file_name in file_list:
-    img = f'''<a><img src="./Pixiv日榜/{list_dir[0]}/{file_name}"  width="200" height="100%" alt="显示出错了"></a>'''
+    img = f'''<a><img src="./Pixiv日榜/{list_dir[0]}/{file_name}"  width="220" height="100%" alt="显示出错了"></a>'''
     readme += img
 #     count += 1
 #     if count % 3 == 0:
