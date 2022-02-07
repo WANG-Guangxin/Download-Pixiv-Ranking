ce223713d6727fb9588580cf685093402aee849c
Guangxin Wang (汪广鑫) <71872849+WANG-Guangxin@users.noreply.github.com>
2022-02-07 17:38:03 +0800

    Update upload_ali.py

--- upload_ali.py
+++ upload_ali.py
@@ -1,6 +1,8 @@
 from aligo import Aligo
 import sys
+import os
 
 if __name__ == '__main__':
     ali = Aligo(refresh_token=sys.argv[1])
-    ali.upload_folder('./Pixiv日榜','6200ab97ef91085b6c1148be84ffb83f67f49b19')
+    list_dir = os.listdir('./Pixiv日榜')
+    ali.upload_folder('./Pixiv日榜/'+list_dir[0],'6200ab97ef91085b6c1148be84ffb83f67f49b19')
